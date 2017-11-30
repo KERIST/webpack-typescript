181993a0ff6a6c3aa631b0d94f0043736590e049
ICX\aliaksandr.nikifarau <alexandernanotexno@gmail.com>
2017-11-30 14:22:09 +0300

    Add jslint support

--- src/script/app.js
+++ src/script/app.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 require('../style/styles.scss');
 
 ReactDOM.render(<div>Hello everyone!</div>, document.querySelector('#app'));
-
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,55 +1,52 @@
 const path = require('path');
-const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: path.resolve(__dirname, './src/script/app.js'),
-    output: {
+	entry: path.resolve(__dirname, './src/script/app.js'),	output: {
 		filename: 'script/bundle.js',
-		path: path.resolve(__dirname, './dist')
+		path: path.resolve(__dirname, './dist'),
 	},
-    resolve: {
+	resolve: {
 		modules: ['node_modules', path.resolve(__dirname, './src')],
 		extensions: ['.js', '.json', '.jsx', '.css', '.scss'],
 		descriptionFiles: ['package.json'],
-	}, 
+	},
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx)?$/,
 				loader: 'babel-loader',
-				//include: path.resolve(__dirname, '/app'),
 				exclude: /(node_modules)/,
 				options: {
-					presets: ['es2015', 'react', 'stage-2'],
+					presets: ['es2015', 'react', 'stage-2']
 				},
 			},
 			{
 				test: /\.(css|scss)?$/,
 				use: ExtractTextPlugin.extract({
 					fallback: 'style-loader',
-					use: [ 'css-loader', 'sass-loader'],
-					publicPath: path.resolve(__dirname, './dist/style')
-				})
-			}
-		]
-    },
-    plugins: [
+					use: ['css-loader', 'sass-loader'],
+					publicPath: path.resolve(__dirname, './dist/style'),
+				}),
+			},
+		],
+	},
+	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/index.html',
-			filename: 'index.html'
+			filename: 'index.html',
 		}),
 		new ExtractTextPlugin({
 			filename: 'style/style.css',
 			disable: false,
-			allChunks: true
-		})
+			allChunks: true,
+		}),
 	],
-    devtool: 'source-map',
+	devtool: 'source-map',
 	target: 'web',
-    cache: false,
-    watchOptions: {
+	cache: false,
+	watchOptions: {
 		aggregateTimeout: 500,
 		poll: true,
 	},
@@ -58,5 +55,5 @@ module.exports = {
 		compress: true,
 		inline: true,
 		port: 7070,
-	},  
+	},
 };
